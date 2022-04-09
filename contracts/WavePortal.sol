3db6fc8b3b5ef15befd66a914f5a93a2700660b1
sajal <asajal@amazon.com>
2022-04-09 15:30:10 +0530

    Creating contract and script to execute it

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor(){
+        console.log("Yo, I am a start contract");
+    }
+}

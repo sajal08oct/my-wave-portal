32ac91c2ee3afb8699cd8c0b7f14c4399f41434b
sajal <asajal@amazon.com>
2022-04-09 15:56:52 +0530

    updating contract to store waves and printing totalwaves received

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,7 +6,20 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    uint256 totalWaves;
+
     constructor(){
         console.log("Yo, I am a start contract");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256){
+        console.log("We have %s total waves!",totalWaves);
+        return totalWaves;
     }
 }
